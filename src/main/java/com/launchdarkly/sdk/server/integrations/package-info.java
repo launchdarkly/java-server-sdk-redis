8bddf7c64e13158d3bd2136fd29ceab23e599c51
Eli Bishop <eli@launchdarkly.com>
2022-01-10 10:55:05 -0800

    don't need package-info

--- src/main/java/com/launchdarkly/sdk/server/integrations/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains {@link com.launchdarkly.sdk.server.integrations.Redis}, the
- * entry point for the LaunchDarkly Java SDK Redis integration.
- * <p>
- * In the LaunchDarkly Java SDK, the {@code integrations} package also contains other
- * classes for configuring how the SDK communicates with other services.
- */
-package integrations;
